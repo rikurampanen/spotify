41b8f7fdfda9e3322cb5b0c5d1f65b181c047019
kuracode <150280854+rikurampanen@users.noreply.github.com>
2024-04-20 14:44:00 +0300

    more features

--- main.py
+++ main.py
@@ -4,16 +4,15 @@ from credentials import my_client_id, my_client_secret
 from ytmusicapi import YTMusic
 
 ytmusic = YTMusic("oauth.json")
-yt_playlist_id = "PLWc3tzTaQV7tIGzHYKhhSPrOm3ec3aVC7"
-spotify_playlist_id = '6ZkwfPHoQFEvxtuXf6dUnr'
-
-# Replace these values with your own credentials
-client_id = my_client_id
-client_secret = my_client_secret
-
+#yt_playlist_id = "PLWc3tzTaQV7tIGzHYKhhSPrOm3ec3aVC7"
+spotify_playlist_id = '37i9dQZEVXbMxcczTSoGwZ'
 # Initialize Spotipy client with credentials
-client_credentials_manager = SpotifyClientCredentials(client_id=client_id, client_secret=client_secret)
+client_credentials_manager = SpotifyClientCredentials(client_id=my_client_id, client_secret=my_client_secret)
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_playlist_info(username, playlist_id):
+    playlist_info = sp.user_playlist(username, playlist_id)
+    return playlist_info
 
 # Function to retrieve playlist tracks
 def get_playlist_tracks(username, playlist_id):
@@ -24,13 +23,38 @@ def get_playlist_tracks(username, playlist_id):
         tracks.extend(results['items'])
     return tracks
 
-# Extract data from the playlist
-playlist_tracks = get_playlist_tracks('', spotify_playlist_id)
+existing_playlists = []
 
-# Print track information
-for track in playlist_tracks:
-    track_name = track['track']['name']
-    artists = ", ".join([artist['name'] for artist in track['track']['artists']])
-    album = track['track']['album']['name']
-    search_results = ytmusic.search(f"{track_name} by {artists} from {album}")
-    ytmusic.add_playlist_items(yt_playlist_id, [search_results[1]['videoId']])
+def check_duplicate_playlist():
+    playlists = ytmusic.get_library_playlists()
+    for playlist in playlists:
+        playlist_title = playlist['title']
+        existing_playlists.append(playlist_title)
+
+def convert_to_ytmusic():
+    # Extract data from the playlist
+    print("Haetaan playlist nimi...")
+    playlist_name = get_playlist_info('', spotify_playlist_id)
+    print("Haetaan playlist kappaleet...")
+    playlist_tracks = get_playlist_tracks('', spotify_playlist_id)
+    total_tracks = len(playlist_tracks)
+    check_duplicate_playlist()
+    if playlist_name['name'] in existing_playlists:
+        print("ÄLÄ PERKELE!")
+        quit()
+    else:
+        yt_playlist_name = ytmusic.create_playlist(playlist_name['name'], '')
+        print("Luodaan playlist: ", playlist_name['name'])
+        i = 1
+        for track in playlist_tracks:
+            track_name = track['track']['name']
+            artists = ", ".join([artist['name'] for artist in track['track']['artists']])
+            search_results = ytmusic.search(f"{track_name} by {artists}")
+            if not search_results:
+                print(f"Haku {track_name} by {artists} ei tuottanut tuloksia")
+            else:
+                print(f"[{i}/{total_tracks}] {track_name} by {artists}")
+                i += 1
+                ytmusic.add_playlist_items(yt_playlist_name, [search_results[1]['videoId']])
+
+convert_to_ytmusic()
